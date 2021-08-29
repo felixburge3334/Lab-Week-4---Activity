4d82a384d32f1540ee7dfbf53f6ebc0a4f98c610
felixburge3334 <felix.h.burge@student.uts.edu.au>
2021-08-29 20:18:12 +1000

    90 percent complete

---
+++ Assets/Scripts/PrintAndHide.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int i = 3;
+    public Renderer rend;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameObject.name + ":" + ++i);
+    }
+}
